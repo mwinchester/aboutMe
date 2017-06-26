e1d416ba38b1d7ee38fb1098e74f0c7044cb83e7
mwinchester <mwinchester@mchenry.edu>
2017-06-26 13:09:30 -0500

    adding an extra comment

--- src/com/meriwin/Main.java
+++ src/com/meriwin/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// Using println
+    // This is me making changes
         System.out.println("Hi! My name is Meri!");
         System.out.println("I like to program. I have been programming since I was nine.");
         System.out.println("I have been working with Java since the late 1990's.");
