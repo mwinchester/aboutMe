908e9589ef34136cf2c3053b5f12e1f50351a705
mwinchester <mwinchester@mchenry.edu>
2017-06-26 13:08:19 -0500

    Initial commit

---
+++ src/com/meriwin/Main.java
@@ -0,0 +1,12 @@
+package com.meriwin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Using println
+        System.out.println("Hi! My name is Meri!");
+        System.out.println("I like to program. I have been programming since I was nine.");
+        System.out.println("I have been working with Java since the late 1990's.");
+        System.out.println("It has changed a lot in that time. ");
+    }
+}
